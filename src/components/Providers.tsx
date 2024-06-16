b829e133e79526ee92f7626578125c28babe5483
noogler-eng <sharadpoddar10102003@gmail.com>
2024-06-17 01:27:13 +0530

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,15 @@
+// making an client side rendering
 "use client";
+
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-type Props = {
-  children: React.ReactNode;
-};
+type Props = { children: React.ReactNode };
 
 const queryClient = new QueryClient();
 
+// 1. passing children as a prop
+// 2. wrapping with QueryClientProvider
 const Providers = ({ children }: Props) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
