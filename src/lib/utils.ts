68c4b508a9cbd1c752e5f6cea5bee26887c9edc9
noogler-eng <sharadpoddar10102003@gmail.com>
2024-06-17 01:42:25 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// 1. taking input as a array ...inputs: ClassValue[] || any
+// 2. merging two tailwind classes but overdies the conflicted classNames
+// 3. clasx also merging
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// 1. converting any string to ASCII code
+// 2. remove non ascii characters, replaing this characters by ""
 export function convertToAscii(inputString: string) {
-  // remove non ascii characters
   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
   return asciiString;
 }
+
+// const isActive = true;
+// const combinedClasses = clsx(baseClasses, {
+//   'bg-green-500': isActive,
+//   'bg-red-500': !isActive,
+// });
