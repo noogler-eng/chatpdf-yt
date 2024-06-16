5dddcda7eacda26fa8dbbe4cbe70534bc08f1dee
noogler-eng <sharadpoddar10102003@gmail.com>
2024-06-17 01:25:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+// main.tsx file for react = layout.tsx in next.js
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -7,15 +9,14 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// setting up the title for project
 export const metadata: Metadata = {
   title: "ChatPDF YT",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// 1. clerkProvider wrapper to wrap whole app
+// 2. Providers wrapper, getting from component
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <Providers>
